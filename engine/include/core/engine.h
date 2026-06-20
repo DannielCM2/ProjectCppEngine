726f6126e4e40b2d8ad0742e3cf160955b15af01
DannielCM2 <danielcanaria9@gmail.com>
2026-06-21 00:09:09 +0800

    "add: fps and frameDelay property to Engine for timing control"

--- engine/include/core/engine.h
+++ engine/include/core/engine.h
@@ -1,8 +1,13 @@
+#include <chrono>
 #pragma once
 
 namespace core {
     class Engine {
     public:
+        std::chrono::duration<double, std::milli> frameDelay;
+        int fps;
+
+        Engine();
         void start();
     };
 }
