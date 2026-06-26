7f09e81c49de84b87a550e214e7c047436a4056b
DannielCM2 <danielcanaria9@gmail.com>
2026-06-26 08:05:44 +0800

    "add: window frame to the game loop"

--- engine/src/core/engine.cpp
+++ engine/src/core/engine.cpp
@@ -2,23 +2,30 @@
 #include <thread>
 #include <chrono>
 #include "core/engine.h"
+#include "core/frame.h"
+#include <windows.h>
 
+// Engine constructor initializes the frame rate and calculates the frame delay based on the desired frames per second (fps).
 core::Engine::Engine() {
     fps = 60;
     frameDelay = std::chrono::duration<double, std::milli>(1000.0 / fps);
 }
 
-void core::Engine::start() {
-    std::cout << "Engine starting..." << std::endl;
+void core::Engine::init() {
+    hwnd = initWindow();
+}
 
+// The start method runs the main loop of the engine, printing messages to indicate the engine's state and managing frame timing to maintain a consistent frame rate.
+void core::Engine::start() {
     bool running = true;
 
     while (running) {
+        processMessages();
+
         auto frameStart = std::chrono::steady_clock::now();
 
-        std::cout << "Engine running..." << std::endl;
-
         auto frameEnd = std::chrono::steady_clock::now();
+
         std::chrono::duration<double, std::milli> elapsed = frameEnd - frameStart;
         double sleepTime = frameDelay.count() - elapsed.count();
 
