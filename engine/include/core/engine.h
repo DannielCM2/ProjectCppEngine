02f6d3b538015e75b1a990103728501cc379d47f
DannielCM2 <danielcanaria9@gmail.com>
2026-06-26 08:04:38 +0800

    "add: Window reference"

--- engine/include/core/engine.h
+++ engine/include/core/engine.h
@@ -1,13 +1,16 @@
-#include <chrono>
 #pragma once
+#include <chrono>
+#include <windows.h>
 
 namespace core {
     class Engine {
     public:
         std::chrono::duration<double, std::milli> frameDelay;
+        HWND hwnd;
         int fps;
 
         Engine();
+        void init();
         void start();
     };
 }
