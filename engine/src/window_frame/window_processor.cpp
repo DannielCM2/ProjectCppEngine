d7875750abc59976da82dc9083420cfd4c1fed60
DannielCM2 <danielcanaria9@gmail.com>
2026-07-05 01:47:22 +0800

    add: key press and release handler

--- engine/src/window_frame/window_processor.cpp
+++ engine/src/window_frame/window_processor.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include "window_frame/window_frame.h"
+#include <iostream>
 
 LRESULT CALLBACK WindowProc(
     HWND hwnd, 
@@ -8,20 +9,37 @@ LRESULT CALLBACK WindowProc(
     LPARAM lParam
 )
 {
+    if (msg == WM_NCCREATE)
+    {
+        auto* cs = reinterpret_cast<CREATESTRUCT*>(lParam);
+        auto* window = static_cast<Engine::WindowFrame*>(cs->lpCreateParams);
+
+        SetWindowLongPtrW(
+            hwnd,
+            GWLP_USERDATA,
+            reinterpret_cast<LONG_PTR>(window)
+        );
+
+        return TRUE;
+    }
+
+    auto* window = reinterpret_cast<Engine::WindowFrame*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
+
     switch (msg)
     {
-        case WM_NCCREATE:
+        case WM_KEYUP:
         {
-            auto* cs = reinterpret_cast<CREATESTRUCT*>(lParam);
-            auto* window = static_cast<Engine::WindowFrame*>(cs->lpCreateParams);
-
-            SetWindowLongPtrW(
-                hwnd,
-                GWLP_USERDATA,
-                reinterpret_cast<LONG_PTR>(window)
-            );
-
-            return TRUE;
+            if (window && window->getInput()) {
+                window->getInput()->onKeyUp(static_cast<std::uint8_t>(wParam));
+            }
+            return 0;
+        }
+        case WM_KEYDOWN:
+        {
+            if (window && window->getInput()) {
+                window->getInput()->onKeyDown(static_cast<std::uint8_t>(wParam));
+            }
+            return 0;
         }
         case WM_DESTROY:
         {
