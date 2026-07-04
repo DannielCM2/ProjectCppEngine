ccdceff9db8c7a770671b026859a8dcec918cbe6
DannielCM2 <danielcanaria9@gmail.com>
2026-07-04 23:32:09 +0800

    refactor: encapsulate Engine state and add subsystem members

--- engine/include/core/engine.h
+++ engine/include/core/engine.h
@@ -1,16 +1,28 @@
 #pragma once
 #include <chrono>
 #include <windows.h>
+#include <input/input.h>
+#include <window_frame/window_frame.h>
 
-namespace core {
+namespace Engine {
     class Engine {
     public:
-        std::chrono::duration<double, std::milli> frameInterval;
-        HWND hwnd;
-        double fps;
+        Input m_input;
+        WindowFrame m_windowFrame;
 
         Engine();
         void init();
         void start();
+        bool isRunning() const { return m_running; };
+        void stop() { m_running = false; };
+        void setFps(double fps);
+        std::chrono::duration<double, std::milli> getFrameInterval() const { return m_frameInterval; };
+
+    private:
+        bool m_running = true;
+        double m_fps;
+        std::chrono::duration<double, std::milli> m_frameInterval;
+
+        void processEvents();
     };
 }
